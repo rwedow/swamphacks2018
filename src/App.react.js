93a33ff32d9c22f3e528402e2774f16102b5e04d
Luke Bickell <luke@bickell.com>
2018-01-20 13:15:27 -0500

    firebase database integration

--- src/App.react.js
+++ src/App.react.js
@@ -1,21 +1,27 @@
 const React = require('react');
-
+const firebase = require('firebase');
 const NavBar = require('./components/NavBar.react');
 const Home = require('./components/Home.react');
+const FirebaseTest = require('./components/FirebaseTest.react');
 
 const styles = require('./styles.css');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: 'home' };
+    this.state = { location: 'home'};
   }
+
 
   renderContent() {
     switch (this.state.location){
       case 'home':
         return (
           <Home />
+        );
+      case 'firebaseTest':
+      return (
+          <FirebaseTest />
         );
     }
   }
@@ -27,6 +33,7 @@ class App extends React.Component {
         <NavBar />
         { this.renderContent() }
       </div>
+
     );
   }
 }
